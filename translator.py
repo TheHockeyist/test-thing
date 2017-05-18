c22ee57d77c1a3312885186c1d1730b9e2e93432
TheHockeyist <thockeyist@gmail.com>
2017-05-18 12:25:36 -0500

    Test.
    Maybe this will work.

--- translator.py
+++ translator.py
@@ -1,5 +1,18 @@
 # English to TestLang
 
+import re
+
+english_string = input("Type some text to translate - e.g. The red and the blue ball sing.")
+
+testlang_string = english_string
+
+testlang_string = re.sub("red", "kra", cyrillic_string)
+testlang_string = re.sub("blue", "ci", cyrillic_string)
+testlang_string = re.sub("the", "-", cyrillic_string)
+testlang_string = re.sub("and", "i", cyrillic_string)
+testlang_string = re.sub("ball", "mach", cyrillic_string)
+testlang_string = re.sub("sing", "pjut", cyrillic_string)
+
 # red = kra
 # blue = ci
 # the = -
