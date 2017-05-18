3e5972c8647ba3c001c7d0103b9ccbf13e3b8bc8
TheHockeyist <thockeyist@gmail.com>
2017-05-18 12:20:54 -0500

    Initial idea.
    Perhaps I can use this to translate languages!

---
+++ translator.py
@@ -0,0 +1,10 @@
+# English to TestLang
+
+# red = kra
+# blue = ci
+# the = -
+# and = i
+# ball = mach
+# sing = pjut
+
+# Translate The red and the blue ball sing, output should be - kra i - ci mach pjut
